51dd8da92ae62d2553fbd7003fb2dfb9a5d1f7ee
mnajunatha <mnajunathyadav96@gmail.com>
2019-11-20 21:36:31 +0530

    Added comment

--- 02_SpringMvc_WebappWithBoot/src/main/java/com/vmtech/Application.java
+++ 02_SpringMvc_WebappWithBoot/src/main/java/com/vmtech/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+	//add comment
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class,args);
 		//System.out.println("server started");
